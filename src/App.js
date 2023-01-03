70b88d0741779239f07df02d7180515532480757
Lidor Repo <lidormalich@gmail.com>
2023-01-03 11:35:25 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ILcars from './components/ILcars/ILcars';
+
+function App() {
+  return (
+    <div className="App">
+      <ILcars></ILcars>
+    </div>
+  );
+}
+
+export default App;
